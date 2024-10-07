19c890f73ee93a2d3f792b8a1fa7f29a73f62855
RwakelingG <richard.wakeling-gentle@uwe.ac.uk>
2024-10-07 19:28:32 +0100

    Basic game working
    No lose condition or menu yet, but the core loop is working :)

--- src/Game.cpp
+++ src/Game.cpp
@@ -10,31 +10,152 @@ Game::Game(sf::RenderWindow& game_window)
 
 Game::~Game()
 {
-
+	delete [] animals;
+	delete [] passports;
+	delete character;
+	delete passport;
 }
 
 bool Game::init()
 {
+	character = new sf::Sprite;
+	passport = new sf::Sprite;
 
-  return true;
+	loadTextures();
+
+	background.setTexture(background_texture);
+
+	character->setTexture(animals[0]);
+	character->setScale(1.8,1.8);
+	character->setPosition(window.getSize().x / 12, window.getSize().y / 12);
+
+	accept.setTexture(accept_texture);
+	reject.setTexture(reject_texture);
+
+	accept_button.setTexture(accept_button_texture);
+	reject_button.setTexture(reject_button_texture);
+
+	passport->setTexture(passports[0]);
+	passport->setScale(0.6, 0.6);
+	passport->setPosition(window.getSize().x / 2, window.getSize().y / 3);
+
+	stampOffsets();
+
+	lives = 5;
+
+	newAnimal();
+
+	return true;
 }
 
 void Game::update(float dt)
 {
+	if (dragged != nullptr)
+	{
+		dragSprite(dragged);
+	}
+	
+	moveStamps();
 
+	
 }
 
 void Game::render()
 {
+	window.draw(background);
+	
 
+	window.draw(*character);
+
+	window.draw(*passport);
+
+	if (passport_accepted)
+	{
+		window.draw(accept);
+	}
+	if (passport_rejected)
+	{
+		window.draw(reject);
+	}
+
+
+
+	if (menu_open)
+	{
+		window.draw(accept_button);
+		window.draw(reject_button);
+	}
 }
 
 void Game::mouseClicked(sf::Event event)
 {
-  //get the click position
-  sf::Vector2i click = sf::Mouse::getPosition(window);
+	//get the click position
+	sf::Vector2i click = sf::Mouse::getPosition(window);
 
 
+}
+
+void Game::mouseButtonPressed(sf::Event event)
+{
+	if (event.mouseButton.button == sf::Mouse::Left)
+	{
+		sf::Vector2i click = sf::Mouse::getPosition(window);
+		sf::Vector2f clickf = static_cast<sf::Vector2f>(click);
+
+
+		if (!menu_open)
+		{
+			sf::Vector2f passport_positionf = passport->getPosition();
+			if (passport->getGlobalBounds().contains(clickf))
+			{
+				dragged = passport;
+				drag_offset = clickf - passport_positionf;
+			}
+		}
+		else
+		{
+			if (accept_button.getGlobalBounds().contains(clickf))
+			{
+				passport_accepted = true;
+				passport_rejected = false;
+				menu_open = false;
+			}
+			else if (reject_button.getGlobalBounds().contains(clickf))
+			{
+				passport_accepted = false;
+				passport_rejected = true;
+				menu_open = false;
+			}
+		}
+		
+	}
+	if (event.mouseButton.button == sf::Mouse::Right)
+	{
+		toggleMenu();
+	}
+}
+
+void Game::mouseButtonReleased(sf::Event event)
+{
+	if (event.mouseButton.button == sf::Mouse::Left)
+	{
+		if (dragged != nullptr)
+		{
+			sf::Vector2f passport_position = dragged->getPosition();
+
+			if (passport_position.x < window.getSize().x / 5
+				&& passport_position.y < window.getSize().y / 5)
+			{
+				returnPassport();
+			}
+
+			dragged = nullptr;
+		}
+	}
+	if (event.mouseButton.button == sf::Mouse::Right)
+	{
+		
+	}
 }
 
 void Game::keyPressed(sf::Event event)
@@ -42,4 +163,165 @@ void Game::keyPressed(sf::Event event)
 
 }
 
+void Game::keyReleased(sf::Event event)
+{
 
+}
+
+void Game::dragSprite(sf::Sprite* sprite)
+{
+	sf::Vector2i mouse_position = sf::Mouse::getPosition(window);
+	sf::Vector2f mouse_positionf = static_cast<sf::Vector2f>(mouse_position);
+
+	sf::Vector2f drag_position = mouse_positionf - drag_offset;
+	sprite->setPosition(drag_position.x,drag_position.y);
+}
+
+void Game::newAnimal()
+{
+	passport_accepted = false;
+	passport_rejected = false;
+
+	int animal_index = rand() % 3;
+	int passport_index = rand() % 3;
+
+	if (animal_index == passport_index)
+	{
+		should_accept = true;
+	}
+	else
+	{
+		should_accept = false;
+	}
+
+	if(character != nullptr)
+		delete character;
+
+	character = new sf::Sprite;
+
+	character->setTexture(animals[animal_index]);
+	character->setScale(1.8, 1.8);
+	character->setPosition(window.getSize().x / 12, window.getSize().y / 12);
+
+	passport->setTexture(passports[passport_index]);
+	passport->setScale(0.6, 0.6);
+	passport->setPosition(window.getSize().x / 2, window.getSize().y / 3);
+
+}
+
+void Game::stampOffsets()
+{
+	float accept_x = passport->getGlobalBounds().width / 2 - accept.getGlobalBounds().width / 2;
+	float accept_y = passport->getGlobalBounds().height / 4 - accept.getGlobalBounds().height / 2;
+	accept_offset.x = accept_x;
+	accept_offset.y = accept_y;
+
+	float reject_x = passport->getGlobalBounds().width / 2 - reject.getGlobalBounds().width / 2;
+	float reject_y = passport->getGlobalBounds().height / 4 - reject.getGlobalBounds().height / 2;
+	reject_offset.x = reject_x;
+	reject_offset.y = reject_y;
+}
+
+void Game::moveStamps()
+{
+	sf::Vector2f passport_position = passport->getPosition();
+	
+	accept.setPosition(passport_position + accept_offset);
+	reject.setPosition(passport_position + reject_offset);
+}
+
+void Game::toggleMenu()
+{
+	if (!menu_open)
+	{
+		menu_open = true;
+
+		sf::Vector2i mouse_position = sf::Mouse::getPosition(window);
+		sf::Vector2f mouse_positionf = static_cast<sf::Vector2f>(mouse_position);
+
+		float reject_y = reject_button.getGlobalBounds().height + 5;
+
+		accept_button.setPosition(mouse_positionf.x + 5, mouse_positionf.y);
+		reject_button.setPosition(mouse_positionf.x + 5, mouse_positionf.y + reject_y);
+	}
+	else
+	{
+		menu_open = false;
+	}
+}
+
+void Game::returnPassport()
+{
+	if (passport_accepted || passport_rejected)
+	{
+		if ((should_accept && passport_rejected) || (!should_accept && passport_accepted))
+		{
+			lives -= 1;
+			std::cout << "Wrong!\n";
+		}
+		else
+		{
+			std::cout << "Right!\n";
+		}
+
+		newAnimal();
+	}
+}
+
+void Game::loadTextures()
+{
+	if (!background_texture.loadFromFile("../Data/WhackaMole Worksheet/background.png"))
+	{
+		std::cout << "Background texture did not load\n";
+	}
+
+	if (!animals[0].loadFromFile("../Data/Images/Document time/penguin.png"))
+	{
+		std::cout << "Penguin texture did not load\n";
+	}
+
+	if (!animals[1].loadFromFile("../Data/Images/Document time/elephant.png"))
+	{
+		std::cout << "Elephant texture did not load\n";
+	}
+
+	if (!animals[2].loadFromFile("../Data/Images/Document time/moose.png"))
+	{
+		std::cout << "Elephant texture did not load\n";
+	}
+
+	if (!passports[0].loadFromFile("../Data/Images/Document time/penguin passport.png"))
+	{
+		std::cout << "Penguin passport texture did not load\n";
+	}
+
+	if (!passports[1].loadFromFile("../Data/Images/Document time/elephant passport.png"))
+	{
+		std::cout << "Elephant passport texture did not load\n";
+	}
+
+	if (!passports[2].loadFromFile("../Data/Images/Document time/moose passport.png"))
+	{
+		std::cout << "Moose passport texture did not load\n";
+	}
+
+	if (!accept_texture.loadFromFile("../Data/Images/Document time/accept.png"))
+	{
+		std::cout << "Accept texture did not load\n";
+	}
+
+	if (!reject_texture.loadFromFile("../Data/Images/Document time/reject.png"))
+	{
+		std::cout << "Reject texture did not load\n";
+	}
+
+	if (!accept_button_texture.loadFromFile("../Data/Images/Document time/accept button.png"))
+	{
+		std::cout << "Accept texture did not load\n";
+	}
+
+	if (!reject_button_texture.loadFromFile("../Data/Images/Document time/reject button.png"))
+	{
+		std::cout << "Reject texture did not load\n";
+	}
+}
--- src/Game.h
+++ src/Game.h
@@ -13,13 +13,60 @@ class Game
   void update(float dt);
   void render();
   void mouseClicked(sf::Event event);
+  void mouseButtonPressed(sf::Event event);
+  void mouseButtonReleased(sf::Event event);
   void keyPressed(sf::Event event);
+  void keyReleased(sf::Event event);
+  void dragSprite(sf::Sprite*);
+  void stampOffsets();
+  void moveStamps();
+  void toggleMenu();
+  void loadTextures();
+  void returnPassport();
+  void newAnimal();
+  
 
  private:
   sf::RenderWindow& window;
-  sf::Sprite ball;
-  sf::Texture ball_texture;
 
+  sf::Sprite* dragged = nullptr;
+  sf::Vector2f drag_offset;
+
+  sf::Sprite* character;
+  sf::Sprite* passport;
+
+  sf::Texture* animals = new sf::Texture[3];
+  sf::Texture* passports = new sf::Texture[3];
+
+  
+  
+  sf::Sprite accept;
+  sf::Texture accept_texture;
+  sf::Vector2f accept_offset;
+
+  sf::Sprite reject;
+  sf::Texture reject_texture;
+  sf::Vector2f reject_offset;
+
+  sf::Sprite accept_button;
+  sf::Texture accept_button_texture;
+  sf::Sprite reject_button;
+  sf::Texture reject_button_texture;
+
+  sf::Sprite background;
+  sf::Texture background_texture;
+
+  bool passport_accepted = false;
+  bool passport_rejected = false;
+
+  bool menu_open = false;
+
+  bool character_visible = true;
+  bool passport_visible = true;
+
+  bool should_accept;
+
+  int lives;
 };
 
 #endif // PLATFORMER_GAME_H
